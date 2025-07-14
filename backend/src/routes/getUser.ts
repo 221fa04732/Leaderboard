f5c280078f7f2c6e5f2d5cf63c3d1f1775fc7473
221fa04732 <221fa04732@gmail.com>
2025-07-14 23:46:24 +0530

    get user api route donr

--- backend/src/routes/getUser.ts
+++ backend/src/routes/getUser.ts
@@ -1,7 +1,36 @@
 import { Request, Response } from "express"
+import { userType } from "../types/userType";
+import { PrismaClient } from "../../src/generated/prisma";
+const prisma = new PrismaClient();
 
-const getUser = (req : Request, res : Response)=>{
-    console.log('get user')
+const getUser = async(req : Request, res : Response)=>{
+    const search = (req.query.search || "") as string;
+    const page = Number(req.query.page) || 0;
+    try{
+        const limit=10;
+        const skip=page*limit     
+        const allUser : userType[] = await prisma.user.findMany({
+            orderBy : {
+                point : "desc"
+            }
+        })
+        const rankUser : userType[]= allUser.map((user, index)=>({
+            ...user,
+            rank : index+1
+        }))
+        const filterUser : userType[]= rankUser.filter(user=>  user.name.toLowerCase().includes(search.toLowerCase())).slice(skip, skip+limit)
+
+        res.status(201).json({
+            user : filterUser,
+            isPrevious : page > 0,
+            isNext : (page+1)*limit < allUser.length
+        })
+    }
+    catch(e){
+        res.status(500).json({
+            msg : "Server Error!"
+        })
+    }
 }
 
 export default getUser;
---
+++ backend/src/types/userType.ts
@@ -0,0 +1,6 @@
+export interface userType {
+    id: string;
+    name: string;
+    point: number;
+    createdAt: Date;
+}
