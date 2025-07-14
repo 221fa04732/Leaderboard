cbe290161e8fc6a0844bc44dce641189a16e7c22
221fa04732 <221fa04732@gmail.com>
2025-07-14 22:47:38 +0530

    user add api route done

--- backend/src/routes/addUser.ts
+++ backend/src/routes/addUser.ts
@@ -1,7 +1,33 @@
 import { Request, Response } from "express"
+import userSchema from "../validate/userSchema";
+import { PrismaClient } from "../../src/generated/prisma";
+const prisma = new PrismaClient();
 
-const addUser = (req : Request, res : Response)=>{
-    console.log('add user')
+const addUser = async(req : Request, res : Response)=>{
+    const data = req.body
+    try{
+        const userValidate = userSchema.safeParse(data)
+        if(!userValidate.success){
+            res.status(400).json({
+                msg : "Invalid Input!"
+            })
+            return;
+        }
+
+        const user =await prisma.user.create({
+            data:{
+                name : data.name
+            }
+        })
+        res.status(201).json({
+            msg : "User Created Sucessfully"
+        })
+    }
+    catch(e){
+        res.status(500).json({
+            msg : "Server Error!"
+        })
+    }
 }
 
 export default addUser;
