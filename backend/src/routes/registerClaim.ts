138ffafa6c54cf9ac574f1dbe0aaa62f56a87b6d
221fa04732 <221fa04732@gmail.com>
2025-07-15 00:02:34 +0530

    claim register api endpoint done

--- backend/src/routes/registerClaim.ts
+++ backend/src/routes/registerClaim.ts
@@ -1,7 +1,38 @@
 import { Request, Response } from "express"
+import { PrismaClient } from "../../src/generated/prisma";
+const prisma = new PrismaClient()
 
 const registerClaim = (req : Request, res : Response)=>{
-    console.log('claim')
+    const { id } = req.body
+    try{
+        const point = Math.floor(Math.random()*10+1);
+        const history = prisma.$transaction([
+            prisma.user.update({
+                data : {
+                    point : {
+                        increment : point
+                    }
+                },
+                where : {
+                    id : id
+                }
+            }),
+            prisma.history.create({
+                data : {
+                    userId : id,
+                    claimPoint : point
+                }
+            })
+        ])
+        res.status(201).json({
+            msg : `${point} point added`
+        })
+    }
+    catch(e){
+        res.status(500).json({
+            msg : "Server Error!"
+        })
+    }
 }
 
 export default registerClaim;
